bb47fb529570215af6c662caedf47ef9eda8dc99
MdRimon29 <mdraisul.i.rimon@gmail.com>
2026-02-27 09:11:07 +0600

    create schemas

---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+# Defines Pydantic models for request/response validation.
+
+from pydantic import BaseModel, Field, EmailStr
+
+class UserRequest(BaseModel):
+    email : EmailStr
+    username : str
+    first_name : str
+    last_name : str
+    hash_password : str
+    role: str
+
+
+
+
+class Token(BaseModel):
+    access_token : str
+    token_type: str
+
+
+class TodoRequest(BaseModel):
+    title : str = Field(..., max_length=256)
+    description : str = Field(..., max_length=512)
+    priority : int = Field(...)
+    completed : bool
+
+
+class Password_Change(BaseModel):
+    password : str
+    new_password : str = Field(min_length=6)
