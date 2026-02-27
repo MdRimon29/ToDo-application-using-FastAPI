0e1f91e8e5faaaddd05963cb404fa2fa356e526f
MdRimon29 <mdraisul.i.rimon@gmail.com>
2026-02-27 09:58:51 +0600

    update for alembic and some works on main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from . database import engine, Base
-from . routers import auth, todo, admin, user
+from . routers import auth, todo, admin, users
 
 Base.metadata.create_all(bind=engine) # this line create database
 
@@ -9,4 +9,4 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todo.router)
 app.include_router(admin.router)
-app.include_router(user.router)
+app.include_router(users.router)
